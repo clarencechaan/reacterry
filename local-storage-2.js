2df195578bc0f0dd43578f852bdd9ca0e4a8a82a
Clarence Chan <clarencechaan@icloud.com>
2023-12-01 10:13:51 -0500

    Problem local-storage-2

---
+++ local-storage-2.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+export const useLocalStorage = (key, initialValue) => {
+  const [value, setValue] = useState(localStorage.getItem(key) || initialValue);
+
+  useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value]);
+
+  return { value, setValue };
+};
+
+const App = () => {
+  const { value, setValue } = useLocalStorage("inputValue", "");
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={value} onChange={handleChange} />
+    </div>
+  );
+};
+
+export default App;
